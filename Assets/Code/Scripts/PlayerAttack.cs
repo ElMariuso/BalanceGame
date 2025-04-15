3c7b234ca5841520b53899cc1df284cedd680fa4
ElMariuso <thiry.marius@gmail.com>
2025-04-15 19:49:38 +0200

    parry

--- Assets/Code/Scripts/PlayerAttack.cs
+++ Assets/Code/Scripts/PlayerAttack.cs
@@ -5,19 +5,24 @@ public class PlayerAttack : MonoBehaviour
 {
     [Header("Action References")]
     [SerializeField] private InputActionReference attackAction;
+    [SerializeField] private InputActionReference parryAction;
 
     private void OnEnable()
     {
         attackAction.action.Enable();
+        parryAction.action.Enable();
         
         attackAction.action.performed += OnAttackPerformed;
+        parryAction.action.performed += OnParryPerformed;
     }
 
     private void OnDisable()
     {
         attackAction.action.Disable();
+        parryAction.action.Disable();
         
         attackAction.action.performed -= OnAttackPerformed;
+        parryAction.action.performed -= OnParryPerformed;
     }
     
     private void OnAttackPerformed(InputAction.CallbackContext context)
@@ -28,5 +33,15 @@ public class PlayerAttack : MonoBehaviour
     private void HandleAttack()
     {
         Debug.Log("YAARRR");
+    }
+
+    private void OnParryPerformed(InputAction.CallbackContext context)
+    {
+        HandleParry();
+    }
+
+    private void HandleParry()
+    {
+        Debug.Log("PARRY");
     }
 }
